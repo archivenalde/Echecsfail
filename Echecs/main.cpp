042d37ef74418e54db3efb5d86d01b43d14b280b
Adlane Ladjal <adlaneladjal@MacBook-Pro-de-Adlane.local>
2017-08-22 19:45:46 +0200

    Initial Commit

---
+++ Echecs/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Echecs
+//
+//  Created by Adlane Ladjal on 22/08/2017.
+//  Copyright © 2017 Adlane Ladjal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
